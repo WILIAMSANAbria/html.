11cf4532ded0be7aa15894f0f0d8cb94969d7bd2
WILIAMSANAbria <williamsanabria05@gmail.com>
2024-09-29 21:49:36 -0500

    Add files via upload

---
+++ cript.js
@@ -0,0 +1,4 @@
+// Este es un archivo JavaScript básico que puedes modificar para añadir interactividad.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página cargada completamente.');
+});
